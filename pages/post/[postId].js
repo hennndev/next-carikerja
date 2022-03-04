431fbe4451cc4ce4cde1513a99396a945cc3dadd
hennndev <hendraputra230897@gmail.com>
2022-03-04 23:46:29 +0700

    bug-fixed

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -18,7 +18,7 @@ const PostDetail = ({data}) => {
     const router = useRouter()
     const { userLogin } = useAuth()
     const [isApply, setIsApply] = useState(false)
-    const checkCandidateExist = data.kandidat.find(data => data.id === userLogin?._id)
+    const checkCandidateExist = data?.data.kandidat.find(data => data.id === userLogin?._id)
 
     const handleApply = () => {
         if(!userLogin) {
@@ -34,7 +34,7 @@ const PostDetail = ({data}) => {
                 <title>Post Detail | JobSeeker</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            {isApply && <ApplyForm dataPost={data} dataUserLogin={userLogin} handleClose={() => setIsApply(false)}/>}
+            {isApply && <ApplyForm dataPost={data?.data} dataUserLogin={userLogin} handleClose={() => setIsApply(false)}/>}
             <PageContainer>
                 <div className='content'>
                     <PageHeader title="Post Detail"/>
@@ -43,29 +43,29 @@ const PostDetail = ({data}) => {
                         <div className="mt-5 px-2">
                             <div className="flex items-center space-x-3">
                                 <div className="w-16 h-16 relative rounded-full">
-                                    <Image src={data?.dataEmployer?.logoProfileURL ? data?.dataEmployer?.logoProfileURL : '/images/blank-profile.png'} layout="fill" objectFit="contain" className="rounded"/>
+                                    <Image src={data?.data?.dataEmployer?.logoProfileURL ? data?.data?.dataEmployer?.logoProfileURL : '/images/blank-profile.png'} layout="fill" objectFit="contain" className="rounded"/>
                                 </div>
-                                <h1 className="text-lg flex-1 font-medium text-gray-700">{data.dataEmployer.namaPerusahaan}</h1>
+                                <h1 className="text-lg flex-1 font-medium text-gray-700">{data?.data?.dataEmployer.namaPerusahaan}</h1>
                             </div>
                             
                             <div className="flex flex-col space-y-3 mt-5">
-                                <h1 className="text-2xl text-gray-600 font-bold">{data.judul}</h1>
-                                <p>{data.deskripsiPekerjaan}</p>
+                                <h1 className="text-2xl text-gray-600 font-bold">{data?.data.judul}</h1>
+                                <p>{data?.data.deskripsiPekerjaan}</p>
                                 <div className='mt-5 flex items-center flex-wrap'>
-                                    {data.kemampuan.map((skill) => (
+                                    {data?.data.kemampuan.map((skill) => (
                                         <p className='text-box' key={skill}>{skill}</p>
                                     ))}
                                 </div>      
                                 <p>Tingkatan Kandidat: {' '}
-                                    <span className="text-gray-600 font-medium">{data.tingkatanKandidat}</span>
+                                    <span className="text-gray-600 font-medium">{data?.data.tingkatanKandidat}</span>
                                 </p>
                                 <p>Sistem Pekerjaan: {' '}
-                                        <span className="text-gray-600 font-medium">{data.sistemPekerjaan}</span>
+                                        <span className="text-gray-600 font-medium">{data?.data.sistemPekerjaan}</span>
                                 </p>
                                 <p>Jangka Waktu: {' '}
-                                    <span className="text-gray-600 font-medium">{data.jangkaWaktu.replaceAll('_', ' ')}</span>
+                                    <span className="text-gray-600 font-medium">{data?.data.jangkaWaktu.replaceAll('_', ' ')}</span>
                                 </p>
-                                <p>Range Gaji: <span className="text-green-600 font-medium">{data.rangeGaji}</span></p>
+                                <p>Range Gaji: <span className="text-green-600 font-medium">{data?.data.rangeGaji}</span></p>
                                 {!userLogin?.namaPerusahaan && (
                                     <div className="flex items-center space-x-3">
                                         <button className={`btn ${checkCandidateExist && 'bg-gray-600 hover:bg-gray-600 cursor-not-allowed'} text-white flex items-center text-sm w-max`} onClick={handleApply}>
@@ -74,7 +74,7 @@ const PostDetail = ({data}) => {
                                         </button>
                                         <span className="text-gray-400 font-bold">||</span>
                                         <p className="bg-gray-600 p-2 rounded-md text-white text-sm h-max w-max">
-                                            {data.kandidat.length} Kandidat
+                                            {data?.data.kandidat.length} Kandidat
                                         </p>
                                     </div>
                                 )}
@@ -82,16 +82,16 @@ const PostDetail = ({data}) => {
                             <div className="mt-10">
                                 <div className="flex-detail">
                                     <h1 className="text-title-detail">Tentang Perusahaan</h1>
-                                    <p className="text-gray-600">{data.dataEmployer.tentangPerusahaan? data.dataEmployer.tentangPerusahaan : '---'}</p>
+                                    <p className="text-gray-600">{data?.data.dataEmployer.tentangPerusahaan? data?.data.dataEmployer.tentangPerusahaan : '---'}</p>
                                 </div>
                                 <div className="flex-detail">
                                     <h1 className="text-title-detail">Alamat Perusahaan</h1>
-                                    <p className="text-gray-600 mt-1">{data.dataEmployer.alamatPerusahaan ? data.dataEmployer.alamatPerusahaan : '---'}</p>
+                                    <p className="text-gray-600 mt-1">{data?.data.dataEmployer.alamatPerusahaan ? data?.data.dataEmployer.alamatPerusahaan : '---'}</p>
                                 </div>
                                 <div className="flex-detail">
                                     <h1 className="text-title-detail">Situs Perusahaan</h1>
                                     <div className="flex flex-col space-y-2">
-                                        {data.dataEmployer.situsPerusahaan.length > 0 ? data.dataEmployer.situsPerusahaan.map(situs => (
+                                        {data?.data.dataEmployer.situsPerusahaan.length > 0 ? data?.data.dataEmployer.situsPerusahaan.map(situs => (
                                             <p className="mt-1 text-blue-500" key={situs}>{situs}</p>
                                         )) : (
                                             <p className="mt-1">---</p>
